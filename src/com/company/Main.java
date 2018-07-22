f4847d97ada7718ecca347367f76eac675d4c92c
Aman Kumar <thisisamank@gmail.com>
2018-07-22 14:54:47 +0530

    .la

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,14 +12,11 @@ public class Main {
         StringLength length = (s) -> s.length();
         System.out.println(length.getLength("Aman"));
         printLambda(length);
-<<<<<<< HEAD
         RunnableExample example = new RunnableExample();
         example.printRunnable();
         exercise = new Unit1Exercise();
         exercise.display();
-=======
         System.out.println();
->>>>>>> master
     }
     public static void printLambda(StringLength length){ // type inference
         System.out.println(length.getLength("AmanKumar"));
